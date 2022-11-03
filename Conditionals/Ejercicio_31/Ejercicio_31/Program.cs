c3a35781624c51015556b2ec16c007ee04d76e66
marckes04 <maresp95@gmail.com>
2022-11-03 00:53:54 -0500

    first commit

---
+++ Conditionals/Ejercicio_31/Ejercicio_31/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio_31
+{
+    //Leer un numero entero y determinar si es igual a 10
+
+    class Programm
+    {
+        static void Main(string[] arg)
+        {
+            int num = 0;
+
+            Console.WriteLine("Inserte un numero");
+            num = int.Parse(Console.ReadLine());
+
+            if (num == 10)
+                Console.WriteLine("El numero es igual a 10");
+            else
+                Console.WriteLine("El numero es diferente a 10");
+        }
+    }
+}
